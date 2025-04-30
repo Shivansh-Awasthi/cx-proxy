6a6983b4768a3149bad25d52fd025c26cb2ac790
Shivansh <109016548+Shivansh-Awasthi@users.noreply.github.com>
2025-04-30 19:14:40 +0530

    index.js

--- cf-proxy/index.js
+++ cf-proxy/index.js
@@ -5,14 +5,15 @@ export default {
     if (url.pathname.startsWith('/api')) {
       const backendUrl = 'https://backend.toxicgames.in' + url.pathname + url.search;
 
+      // Clone original request but change the destination URL
       const modifiedRequest = new Request(backendUrl, {
         method: request.method,
         headers: request.headers,
-        body: request.method !== 'GET' && request.method !== 'HEAD' ? await request.blob() : undefined,
+        body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : null,
         redirect: 'follow',
       });
 
-      return await fetch(modifiedRequest);
+      return fetch(modifiedRequest);
     }
 
     return new Response('Not found', { status: 404 });
